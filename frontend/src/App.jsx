14f40923e519b6f0b8bc5cf112127acd34b62dee
utkarshYadav21 <utkarshyadav329@gmail.com>
2024-03-06 22:09:06 +0530

    login-register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import "./App.css";
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
+import Users from "./components/Users";
 
 function App() {
+  const [isSignup, setIsSignup] = useState(false);
+
+  const toggleForm = () => {
+    setIsSignup(!isSignup);
+  };
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    <div className="full-container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element />
+          <Route path="/login" element={<Users />} />
+          <Route path="/signup" element={<Users />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Users.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "../css/users.css";
+import { Link } from "react-router-dom";
+
+const Users = () => {
+  const [isSignup, setIsSignup] = useState(false);
+
+  const toggleForm = () => {
+    setIsSignup(!isSignup);
+  };
+
+  return (
+    <div className={`container ${isSignup ? "right-panel-active" : ""}`}>
+      <div className="form-container sign-up-container">
+        <form>
+          <h1>Create Account</h1>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+
+          <button>SignUp</button>
+        </form>
+      </div>
+      <div className="form-container sign-in-container">
+        <form>
+          <h1>Login</h1>
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <a href="#">Forgot Your Password</a>
+          <button>Login</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>
+              To keep connected with us please login with your personal info
+            </p>
+            <Link to="/login">
+              <button className="ghost" onClick={toggleForm}>
+                Sign In
+              </button>
+            </Link>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your details and start the journey with us</p>
+            <Link to="/signup">
+              <button className="ghost" onClick={toggleForm}>
+                Sign Up
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
