8308a01e6e8863b5e0d93f61c177dc60f3d09907
utkarshYadav21 <utkarshyadav329@gmail.com>
2024-03-07 00:12:11 +0530

    nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { BrowserRouter ,Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./components/Users";
 import RecipeCard from "./components/RecipeCard";
 import RecipeCardList from "./components/RecipeCardList";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="full-container">
       <BrowserRouter>
+        <Nav />
         <Routes>
-          <Route path="/" element />
+          <Route path="/" element={<Home />} />
           <Route path="/card" element={<RecipeCard />} />
           <Route path="/cardList" element={<RecipeCardList />} />
           <Route path="/login" element={<Users />} />
