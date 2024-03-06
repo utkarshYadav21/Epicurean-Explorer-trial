1d8bfa325ba41e94008ee94bf7a714de3cfec580
utkarshYadav21 <utkarshyadav329@gmail.com>
2024-03-07 00:38:15 +0530

    changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-
-const Home=()=>{
-    return(
-        <div>
-            
+import "../css/Home.css"
+const Home = () => {
+  return (
+    <div>
+      <h1>search among 1500 recipes</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen b
+      </p>
+      <div className="searching">
+        <h4 style={{ width: "98%", textAlign: "center", marginTop: "30px" }}>
+          What are you looking for
+        </h4>
+        <div className="search-div">
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={searchPets}
+            className="search-field"
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
