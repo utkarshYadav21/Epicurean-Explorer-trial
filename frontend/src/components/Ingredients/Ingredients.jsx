e886967ee8ae653787088c84406ab9221d9b08fb
utkarshYadav21 <utkarshyadav329@gmail.com>
2024-03-08 15:16:33 +0530

    ingredients

--- frontend/src/components/Ingredients/Ingredients.jsx
+++ frontend/src/components/Ingredients/Ingredients.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
 import styles from "./ingredient.module.css";
 
 const scrollToInstructions = () => {
-    const instructionsSection = document.getElementById('instructionsSection');
-    instructionsSection.scrollIntoView({ behavior: 'smooth' });
-  };
-  
+  const instructionsSection = document.getElementById("instructionsSection");
+  instructionsSection.scrollIntoView({ behavior: "smooth" });
+};
+
 const Ingredients = () => {
-   return (
+  const [selectedIngredient,setSelectedIngredient]=useState('');
+  return (
     <div className={styles.galileoDesign}>
       <main className={styles.depth0Frame0}>
-      <section className={styles.frameSave}>
+        <section className={styles.frameSave}>
           <div className={styles.grilledSalmonWithAvocadoSa}>
             <div className={styles.grilledSalmonWithAvocadoSaInner}>
-              <div className={styles.depth6Frame0Parent}>
+              <div className={styles.depth6Frame0Parent} >
                 <img
                   className={styles.depth6Frame05}
                   loading="lazy"
@@ -55,7 +56,10 @@ const Ingredients = () => {
             </div>
             <div className={styles.depth4Frame1Parent}>
               <div className={styles.depth4Frame12}>
-                <button className={styles.depth5Frame02} onClick={scrollToInstructions}>
+                <button
+                  className={styles.depth5Frame02}
+                  onClick={scrollToInstructions}
+                >
                   <div className={styles.depth6Frame06}>
                     <div className={styles.depth7Frame04}>
                       <b className={styles.jumpToRecipe}>Jump to Recipe</b>
@@ -70,164 +74,40 @@ const Ingredients = () => {
                   </div>
                 </div>
               </div>
-              <div className={styles.depth4Frame3}>
-                <div className={styles.depth5Frame04}>
-                  <div className={styles.depth6Frame08}>
-                    <div className={styles.depth7Frame05}>
-                      <div className={styles.depth8Frame04}>
-                        <img
-                          className={styles.depthFrameDepthFrameDepth}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth9Frame03} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth6Frame1}>
-                    <div className={styles.depth7Frame06}>
-                      <div className={styles.salmonFillets}>Salmon fillets</div>
+
+              <div className={styles.depth5Frame5} style={{cursor:"pointer"}} onClick={()=>{
+                console.log("clicked")
+              }}>
+                <div className={styles.depth6Frame013}>
+                  <div className={styles.depth7Frame015}>
+                    <div className={styles.depth7Frame025}>
+                      <img
+                        className={styles.createSalsaIcon}
+                        alt=""
+                        src="../../../images/plus.svg"
+                      />
+                      <div className={styles.depth8Frame018} />
                     </div>
                   </div>
                 </div>
-                <div className={styles.depth5Frame12}>
-                  <div className={styles.depth6Frame09}>
-                    <div className={styles.depth7Frame07}>
-                      <div className={styles.depth8Frame05}>
-                        <img
-                          className={styles.vector02}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth9Frame04} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth6Frame11}>
-                    <div className={styles.depth7Frame08}>
-                      <div className={styles.oliveOil}>Olive oil</div>
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.depth5Frame2}>
-                  <div className={styles.depth6Frame010}>
-                    <div className={styles.depth7Frame09}>
-                      <div className={styles.depth8Frame06}>
-                        <img
-                          className={styles.vector03}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth9Frame05} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth6Frame12}>
-                    <div className={styles.depth7Frame010}>
-                      <div className={styles.salt}>Salt</div>
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.depth5Frame3}>
-                  <div className={styles.depth6Frame011}>
-                    <div className={styles.depth7Frame011}>
-                      <div className={styles.depth8Frame07}>
-                        <img
-                          className={styles.vector04}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth9Frame06} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth6Frame13}>
-                    <div className={styles.depth7Frame012}>
-                      <div className={styles.blackPepper}>Black pepper</div>
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.depth5Frame4}>
-                  <div className={styles.depth6Frame012}>
-                    <div className={styles.depth7Frame013}>
-                      <div className={styles.depth8Frame08}>
-                        <img
-                          className={styles.vector05}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth9Frame07} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth6Frame14}>
-                    <div className={styles.depth7Frame014}>
-                      <div className={styles.avocado}>Avocado</div>
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.depth5Frame5}>
-                  <div className={styles.depth6Frame013}>
-                    <div className={styles.depth7Frame015}>
-                      <div className={styles.depth8Frame09}>
-                        <img
-                          className={styles.vector06}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth9Frame08} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth6Frame15}>
-                    <div className={styles.depth7Frame016}>
-                      <div className={styles.redOnion}>Red onion</div>
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.depth5Frame6}>
-                  <div className={styles.depth6Frame014}>
-                    <div className={styles.depth7Frame017}>
-                      <div className={styles.depth8Frame010}>
-                        <img
-                          className={styles.vector07}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth9Frame09} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth6Frame16}>
-                    <div className={styles.depth7Frame018}>
-                      <div className={styles.freshParsley}>Fresh parsley</div>
-                    </div>
+                <div className={styles.depth6Frame15}>
+                  <div className={styles.depth7Frame016}>
+                    <div className={styles.redOnion}>Red onion</div>
                   </div>
                 </div>
               </div>
               <div className={styles.recipejumpframe}>
-              <div className={styles.depth4Frame4} id="instructionsSection">
-  <div className={styles.depth5Frame05}>
-    <div className={styles.depth6Frame015}>
-      <b className={styles.instructions}>Instructions</b>
-    </div>
-  </div>
-</div>
-
+                <div className={styles.depth4Frame4} id="instructionsSection">
+                  <div className={styles.depth5Frame05}>
+                    <div className={styles.depth6Frame015}>
+                      <b className={styles.instructions}>Instructions</b>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className={styles.oliveOilDrop}>
                 <div className={styles.depthFrameDepthFrameVector}>
                   <div className={styles.depth5Frame06}>
-                    <div className={styles.depth6Frame016}>
-                      <div className={styles.depth7Frame019}>
-                        <img
-                          className={styles.createSalsaIcon}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth8Frame011} />
-                      </div>
-                    </div>
                     <div className={styles.depth6Frame17}>
                       <div className={styles.depth7Frame020}>
                         <div className={styles.depth8Frame012}>
@@ -242,86 +122,6 @@ const Ingredients = () => {
                             Drizzle the salmon with olive oil and sprinkle with
                             salt and black pepper
                           </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth5Frame13}>
-                    <div className={styles.depth6Frame017}>
-                      <div className={styles.depth7Frame021}>
-                        <img
-                          className={styles.vector08}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth8Frame014} />
-                      </div>
-                    </div>
-                    <div className={styles.depth6Frame18}>
-                      <div className={styles.depth7Frame022}>
-                        <div className={styles.depth8Frame015}>
-                          <b className={styles.preheatGrillTo}>
-                            Preheat grill to medium-high heat
-                          </b>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth5Frame21}>
-                    <div className={styles.depth6Frame018}>
-                      <div className={styles.depth7Frame023}>
-                        <img
-                          className={styles.vector09}
-                          alt=""
-                          src="../../../images/plus.svg"
-                        />
-                        <div className={styles.depth8Frame016} />
-                      </div>
-                    </div>
-                    <div className={styles.depth6Frame19}>
-                      <div className={styles.depth7Frame024}>
-                        <div className={styles.depth8Frame017}>
-                          <b className={styles.grillTheSalmon}>
-                            Grill the salmon
-                          </b>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth5Frame31}>
-                    <div className={styles.depth6Frame019}>
-                      <div className={styles.depth7Frame025}>
-                        <input className={styles.vector010} type="checkbox" />
-                        <div className={styles.depth8Frame018} />
-                      </div>
-                    </div>
-                    <div className={styles.depth6Frame110}>
-                      <div className={styles.depth7Frame026}>
-                        <div className={styles.depth8Frame019}>
-                          <b className={styles.makeTheAvocado}>
-                            Make the avocado salsa
-                          </b>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.depth5Frame41}>
-                    <div className={styles.depth6Frame020}>
-                      <div className={styles.depth7Frame027}>
-                        <img
-                          className={styles.vector011}
-                          alt=""
-                          src="../../../images/vector--0-14.svg"
-                        />
-                        <div className={styles.depth8Frame020} />
-                      </div>
-                    </div>
-                    <div className={styles.depth6Frame111}>
-                      <div className={styles.depth7Frame028}>
-                        <div className={styles.depth8Frame021}>
-                          <b className={styles.serveTheGrilled}>
-                            Serve the grilled salmon with the avocado salsa
-                          </b>
                         </div>
                       </div>
                     </div>
