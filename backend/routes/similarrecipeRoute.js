80919f98f8906fa6caa411752e6e0fd2d1791c8d
Moksh <moksh2004jain@gmail.com>
2024-03-09 04:31:19 +0530

    Added Search Recipe by similarity

--- backend/routes/similarrecipeRoute.js
+++ backend/routes/similarrecipeRoute.js
@@ -4,7 +4,7 @@ const recipeController = require('../controller/recipeController')
 const router = express.Router()
 
 router.route('/')
-    .post(recipeController.getSimilarRecipe)
+    .post(recipeController.getsimilarRecipe)
 
 
 module.exports = router
